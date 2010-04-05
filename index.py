d273c575a78e9e107449c43ba86f1028beab6c97
Reece Hart <reece@invitae.com>
2010-04-05 15:56:10 -0700

    moved logic from index.py to separate Guesser.py module

--- index.py
+++ index.py
@@ -1,3 +1,14 @@
-print 'Content-Type: text/plain'
-print ''
-print 'Hello there Reece'
+import wsgiref.handlers
+from google.appengine.ext import webapp
+from Guesser import Guesser
+
+
+def main():
+    application = webapp.WSGIApplication([
+            ('/.*', Guesser)],
+            debug=True)
+    wsgiref.handlers.CGIHandler().run(application)
+
+
+if __name__ == '__main__':
+    main()

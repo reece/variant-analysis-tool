df1224f988624e4e37bd6b243558066161316ac9
Reece Hart <reece@invitae.com>
2010-06-04 11:50:10 -0700

    moved appengine code to appengine/; intent is that it will be one client of the library in the top level

--- lib/GenomeCommons/VariantAnalyzer.py
+++ lib/GenomeCommons/VariantAnalyzer.py
@@ -4,56 +4,31 @@ import shelve
 import Bio.Entrez
 import Bio.SeqIO
 
+import GenomeCommons.Exception as Exception
 import GenomeCommons.HGVSVarSpec
+from GenomeCommons.utils import ( ac_to_gi, fetch_gi_as_seq )
 
 
 Bio.Entrez.email = 'reece@berkeley.edu'
-Bio.Entrez.tool = 'Genome Commons > Variant Analysis Tool'
+Bio.Entrez.tool = __file__
 #os.linesep = '\n'
 
 
 class VariantAnalyzer(object):
 	def __init__(self,vartxt):
 		self.vs = GenomeCommons.HGVSVarSpec.HGVSVarSpec(vartxt)
-		self.shelf = shelve.open('/tmp/cache')
 
-	def __del__(self):
-		self.shelf.close()
-		# FIXME: can't figure out how to call super's __del__:
-		# super(self.__class__,self).__del__()
+	def print_summary(self):
+		vs = self.vs
+		print '%s = %s / %s / %s' % (vs, vs.accession, vs.type, vs.varlist)
 
-	def _fetch_nucleotide(self,ac):
-		skey = 'entrez/' + ac
-		if not self.shelf.has_key(skey):
-			handle = Bio.Entrez.esearch(db='nucleotide',term=ac)
-			self.shelf[skey] = Bio.Entrez.read(handle)
-			print 'fetched and wrote %s' % (skey)
-			handle.close()
-		else:
-			print 'fetched %s from cache ' % (skey)
-		return self.shelf[skey]
+		gi = ac_to_gi(vs.accession)
+		r = fetch_gi_as_seq(gi)
 
-	def dome(self):
-		print self._fetch_nucleotide(self.vs.accession)
-
-
-#if int(record['Count']) > 1:
-#	self.response.out.write(
-#		self._render( { 'error': 'Error: '+record['Count']+' records returned.' } )
-#		)
-#	return
-#
-#gi = record['IdList'][0]
-
-#	gi = 238018044
-#
-#	handle = Entrez.efetch(db='nucleotide', id=gi, rettype='gb')
-#	gb = SeqIO.parse(handle,'genbank').next()
-#
-#	data = { 'gb': gb, 'gi': gi, 'vs': vs, 'description': gb.description }
-#	self.response.out.write( self._render( data ) )
-#
-#	handle.close()
-#	return
-
-
+		print 'id:%s, gi:%s' % (r.id, gi)
+		print 'description:%s' % (r.description)
+		print '#features:%d' % (len(r.features))
+		for f in r.features:
+			print '####################'
+			print f
+		
